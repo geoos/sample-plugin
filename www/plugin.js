e168b125b227e8c48be4d8c000923c8ce4e0a809
Jota <otrojotaj@gmail.com>
2021-05-12 18:34:49 -0400

    update

--- www/plugin.js
+++ www/plugin.js
@@ -1,6 +1,10 @@
 class GEOOSDemoPlugin extends GEOOSPlugIn {
     get code() {return "demo"}    
-    get includeFiles() {return ["analysis/calculador-superficie.js", "tools/histograma-variable.js"]}
+    get includeFiles() {return [
+        "analysis/calculador-superficie.js", 
+        "tools/histograma-variable.js",
+        "raster-visualizers/dataPoints/data-points.js", "raster-visualizers/dataPoints/DataPointsVisualizer.js" 
+    ]}
 }
 
 (new GEOOSDemoPlugin())
